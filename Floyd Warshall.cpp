2f803a80e3ad21313476698aa0ad81b87e99036b
Abhay Singh Chauhan <chauhanabhay1248@gmail.com>
2021-10-03 21:43:46 +0530

    Added

---
+++ Floyd Warshall.cpp
@@ -0,0 +1,23 @@
+//adj[u] = [v, cost]
+//adj[v] = [u, cost]
+//inf = 1e18
+void floydWarshall(int n, vector <vector<int>> &dist, vector <pair<int, int>> &adj) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            dist[i][j] = inf;
+        }
+        dist[i][i] = 0; // i to i, dist = 0
+    }
+    for (int i = 0; i < n; ++i) {
+        for (auto j: adj[i]) {
+            dist[i][j.fi] = min(dist[i][j.fi], j.se);
+        }
+    }
+    for (int k = 0; k < n; ++k) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+            }
+        }
+    }
+}
