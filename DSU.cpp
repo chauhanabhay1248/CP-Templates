6263b95171d970e7b8bbbc850e599cf83444bfb8
Abhay Singh Chauhan <chauhanabhay1248@gmail.com>
2021-10-03 16:06:59 +0530

    Added

--- DSU.cpp
+++ DSU.cpp
@@ -2,24 +2,24 @@
 struct DSU {
     vector<int> par;
     vector<int> sze;
-    int connected;
+    int connected; // stores number of connected components
 
-    void init(int n) {
+    void init(int n) { // initialize DSU
         par.resize(n + 1);
         sze.assign(n + 1, 1);
         iota(all(par), 0);
         connected = n;
     }
 
-    int get(int x) {
+    int get(int x) { // get parent
         return (par[x] = (par[x] == x ? x : get(par[x])));
     }
 
-    int getSize(int u) {
+    int getSize(int u) { // get size of component
         return sze[get(u)];
     }
 
-    void un(int x, int y) {
+    void un(int x, int y) { // unite two components
         x = get(x);
         y = get(y);
         if (x == y) {
