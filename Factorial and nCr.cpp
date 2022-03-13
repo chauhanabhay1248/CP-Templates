b6ccab311c4cdda1e14e3df10e822425ce8581f6
Abhay Singh Chauhan <54814128+chauhanabhay1248@users.noreply.github.com>
2022-03-13 16:28:24 +0530

    Added

--- Factorial and nCr.cpp
+++ Factorial and nCr.cpp
@@ -1,43 +1,46 @@
+constexpr long long mod = 1e9 + 7;
+constexpr long long MOD = 998244353;
+
+// Power under mod (a ^ b) % mod
+int modpow(int a, int b, int m = mod) {
+    a = a & m; 
+    int ans = 1;
+    while (b) {
+        if (b & 1) { 
+            ans = (ans * a) % m; 
+        }
+        b = b >> 1; 
+        a = (a * a) % m;
+    }
+    return ans;
+}
+ 
+// Inverse Mod (1 / a) % mod
+int modinv(int a, int m = mod) { 
+    return modpow(a, m - 2); 
+}
+
 const int N = 2e5 + 7;
+vector<int> fac;
 
-vector<int> fact;
-vector<int> invfact;
-vector<int> inv;
-
-// Number Inverse
-void numInv() {
-    inv.resize(N);
-    inv[0] = 1;
-    inv[1] = 1;
-    for (int i = 2; i < N; ++i) {
-        inv[i] = mod - (inv[mod % i] * (mod / i) % mod);
-    }
-}
-
-// Factorial Inverse
-void factInv() {
-    invfact.resize(N);
-    invfact[0] = 1;
-    invfact[1] = 1;
-    for (int i = 2; i < N; ++i) {
-        invfact[i] = (inv[i] * invfact[i - 1]) % mod;
-    }
-}
-
-// Factorial
 void fact() {
-    fact.resize(N);
-    fact[0] = 1;
-    fact[1] = 1;
+    fac.resize(N);
+    fac[0] = 1;
+    fac[1] = 1;
     for (int i = 2; i < N; ++i) {
-        fact[i] = (fact[i - 1] * i) % mod;
+        fac[i] = (fac[i - 1] * i) % mod;
     }
 }
 
-// nCr [n! / (r! * (n - r)!)]
-int nCr(int n, int r) {
-    if (r > n) {
+// nCr % m
+int nCr(int n, int r, int m = mod) {
+    if(n < r) {
         return 0;
     }
-    return (((fact[n] * invfact[r]) % mod) * invfact[n - r]) % mod;
+    if(r == 0) {
+        return 1;
+    }
+    int num = (fac[n] * modinv(fac[r], m)) % m;
+    int den = modinv(fac[n - r], m) % m;
+    return (num * den) % m;
 }
